74ca6220155a82f5f35cea3aefdaa72faaba06a0
naim71 <naimkhan1314@gmail.com>
2023-02-01 16:46:36 +0600

    final push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+// responsive
+const menuHamburger = document.querySelector(".hamburger")
+const navLinks = document.querySelector(".navbar")
+
+menuHamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu')
+})
+
+
 // form validation
-function validation(){
+function validation() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email');
     const subject = document.getElementById('subject');
@@ -14,3 +23,24 @@ function validation(){
     alert('Form Submitted Succesfully!')
     return true;
 }
+
+//projects counting
+
+function numCounter(tagId, maxCount, speed) {
+    let initialNumber = 0;
+    function counter() {
+        document.getElementById(tagId).innerHTML = initialNumber;
+        ++initialNumber;
+    }
+    let counterDelay = setInterval(counter, speed);
+    function totalTime() {
+        clearInterval(counterDelay);
+    }
+    const totalPeriod = speed * (maxCount);
+    setTimeout(totalTime, totalPeriod);
+}
+
+numCounter("customer", 1500, 5);
+numCounter("team", 12, 400);
+numCounter("projects", 436, 10);
+numCounter("experience", 20, 100);
